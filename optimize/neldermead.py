672224ac73722e7310ec6c4a980162e926ca36f4
Bryson Cale <bryson.cale1@gmail.com>
2022-02-14 07:13:22 -0800

    Small tweaks to NM

--- optimize/neldermead.py
+++ optimize/neldermead.py
@@ -99,7 +99,6 @@ class IterativeNelderMead:
         # The current status
         self.status = "failed"
             
-
     def initialize_parameters(self, p0, lower_bounds=None, upper_bounds=None, parameter_names=None, vary=None):
         """Initialize the remaining solver options with the initial parameters.
 
@@ -381,6 +380,7 @@ class IterativeNelderMead:
         if subspace_index is not None:
             n = len(self.subspaces[subspace_index])
             inds = self.subspace_inds[subspace_index]
+            #breakpoint()
             simplex = np.zeros((n, n+1))
             pmin = self.pmin.unpack(keys='value')['value'][inds]
             pinit = self.p0_numpy['value'][inds]
